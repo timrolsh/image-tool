2f278676d65dd301a61699cd9139aef3cc917bc2
Nico <nico.kichukov@outlook.com>
2023-12-14 20:38:26 -0500

    Simple front end dont think it works yet

---
+++ front_end.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import mainScript as ms
+
+    
+st.write('# Skim Image Tool!')
+    
+
+st.write(' --- ')
+
+left_column, middle_column, right_column = st.columns(3)
+with left_column:    
+    st.write('###  *Instructions*  ')
+    st.write('Step 1: Run setting')
+    st.write('Step 2: Put image files in the newly created input folder')
+    st.write('Step 3: Run setting again')
+    st.write("Step 4: Put in what settings you want in the newly created Input_setting.xlsx file and save (Ctrl+S) when you're done")
+    st.write('Step 5: Click the Run button under Setting and images will be in the output folder!')
+
+
+
+right_column.button("Setting", "setting", on_click= ms.Setting())
+right_column.button("Run","run", on_click= ms.Run())
