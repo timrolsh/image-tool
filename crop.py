d45697b240240734a394c49e57ac2e50f8b6cdf9
Nico <nico.kichukov@outlook.com>
2024-01-22 20:10:28 -0500

    added outlines to the image after finally 'closing off' the guassian blur outline

--- crop.py
+++ crop.py
@@ -8,14 +8,23 @@ image_gray =  cv2.imread(r'cosmetic.jpg' , cv2.IMREAD_GRAYSCALE)
 b,g,r =  cv2.split(image)
 image2 =  cv2.merge([r,g,b])
  
-blur =  cv2.GaussianBlur(image_gray, ksize= ( 5 , 5 ), sigmaX= 0 )
+blur =  cv2.GaussianBlur(image_gray, ksize= ( 5 , 5 ), sigmaX= 0 ) # the ksize value dictates the strength of the blur.
 ret, thresh1 =  cv2.threshold(blur,  127 ,  255 , cv2.THRESH_BINARY)
 
 edged =  cv2.Canny(blur,  10 ,  250 )
 cv2.imshow( 'Edged' , edged)
 
+kernel =  cv2.getStructuringElement(cv2.MORPH_RECT, ( 7 , 7 ))
+closed =  cv2.morphologyEx(edged, cv2.MORPH_CLOSE, kernel) # close off any "holes" in the outline
+cv2.imshow( 'closed' , closed)
 
 cv2.imshow( 'image' , image)
+
+contours, _ =  cv2.findContours(closed.copy(),cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+total =  0
+contour_image =  cv2.drawContours(image, contours, - 1 , ( 0 , 255 , 0 ),  3 )
+cv2.imshow( 'contours_image' , contour_image)
+
 
  
 cv2.waitKey( 0 )
