c12d79d5e51fb5bdf8bb89d09f4d836cf3eb98ed
Gible147 <nico.kichukov@outlook.com>
2024-05-01 11:36:39 -0400

    Fixed crop inhomogenous error

--- crop.py
+++ crop.py
@@ -1,7 +1,9 @@
 import cv2
 import numpy as np
 import os
+
 # input/3077207647.jpeg
+
 
 def crop_image(INPUT, kSize: int):
     # ---- Guassian Blur ---- #
@@ -24,25 +26,28 @@ def crop_image(INPUT, kSize: int):
     # cv2.imshow( 'closed' , closed)
 
     # Show original for reference
-    #cv2.imshow("image", image)
+    # cv2.imshow("image", image)
 
     # ---- Contour the outline on based off the original image ---- #
     contours, _ = cv2.findContours(
         closed.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
     )
+
     total = 0
     contour_image = cv2.drawContours(image, contours, -1, (0, 255, 0), 3)
     cv2.imshow("contours_image", contour_image)
 
     # ---- Find coordinates of closest 4 corners (square) around the outline of the image ---- #
-    contours_xy = np.array(contours)
-    contours_xy.shape
+    contours_xy = [np.array(cnt) for cnt in contours]
+    # contours_xy.shape
 
     x_min, x_max = 0, 0
     value = list()
     for i in range(len(contours_xy)):
         for j in range(len(contours_xy[i])):
-            value.append(contours_xy[i][j][0][0])  # Value of x when fourth parenthesis is 0
+            value.append(
+                contours_xy[i][j][0][0]
+            )  # Value of x when fourth parenthesis is 0
             x_min = min(value)
             x_max = max(value)
     print(x_min)
@@ -53,7 +58,9 @@ def crop_image(INPUT, kSize: int):
     value = list()
     for i in range(len(contours_xy)):
         for j in range(len(contours_xy[i])):
-            value.append(contours_xy[i][j][0][1])  # Value of x when fourth parenthesis is 0
+            value.append(
+                contours_xy[i][j][0][1]
+            )  # Value of x when fourth parenthesis is 0
             y_min = min(value)
             y_max = max(value)
     print(y_min)
@@ -68,5 +75,5 @@ def crop_image(INPUT, kSize: int):
     cv2.imwrite("org_trim.jpg", img_trim)
     org_image = cv2.imread("org_trim.jpg")
 
-    os.remove("org_trim.jpg")    
+    os.remove("org_trim.jpg")
     return org_image
